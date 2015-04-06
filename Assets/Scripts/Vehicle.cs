c94c136394fbc772c6b4a2fc87563be6623f85ec
sampsoninc916 <warofprogramming@gmail.com>
2015-04-06 18:10:03 -0400

    Added Vehicle Class

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+enum VehicleType
+{
+    Car, Truck, Bus
+}
+
+public class Vehicle
+{
+    private int speed;
+    private double distance;
+    private String type;
+    
+    public Vehicle()
+	{
+	}
+
+    public void Run()
+    {
+        while (true)
+        {
+
+        }
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public int setSpeed(int newSpeed)
+    {
+        speed = newSpeed;
+        return speed;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public double setDistance(double newDistance)
+    {
+        distance = newDistance;
+        return distance;
+    }
+
+    public String getVehicleType()
+    {
+
+    }
+}
